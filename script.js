4a37daee4afcabdabbba6c312d21c3698a0a2e1c
MiguelMartoni <miguelmartoni26@gmail.com>
2024-12-03 20:31:00 -0300

    Adicionando função de filtro

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ async function carregarProdutos() {
   try {
       const sortOptions = document.getElementById('sort-options').value;
       const [sortField, sortDirection] = sortOptions.split('-');
-      const sortByPrice = sortField === 'preco';
+      const sortByPrice = sortField == 'preco';
+      const filter = document.getElementById('search-bar').value;
+      const searchButton = document.getElementById('search-button');
 
+      searchButton.addEventListener('click', () => {
+        filter = document.getElementById('search-bar').value;
+    });
     
-      const response = await fetch(`http://localhost:8080/produto?sortField=${sortField}&sortDirection=${sortDirection}&sortByPrice=${sortByPrice}`);
+      const response = await fetch(`http://localhost:8080/produto?sortField=${sortField}&sortDirection=${sortDirection}&sortByPrice=${sortByPrice}&filter=${filter}`);
       const products = await response.json();
 
       const productList = document.getElementById('product-list');
@@ -21,8 +26,9 @@ async function carregarProdutos() {
               <div class="card h-100 bg-dark text-white shadow">
                   <img src="${product.imagemURL}" class="card-img-top" alt="Imagem do produto">
                   <div class="card-body">
-                      <h5 class="card-title">${product.nome}</h5>
-                      <p class="card-text">Preço: R$ ${product.preco.toFixed(2)}</p>
+                    <h5 class="card-title">${product.nome}</h5>
+                    <p class="card-text">${product.descricao}</p>
+                    <p class="card-text">Preço: R$ ${product.preco}</p>
                   </div>
               </div>
           `;
@@ -37,3 +43,5 @@ async function carregarProdutos() {
 document.getElementById('sort-options').addEventListener('change', carregarProdutos);
 
 window.onload = carregarProdutos;
+
+
